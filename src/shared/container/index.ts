571e394c7d36c40164e01ee42a531cc1b452a8bd
Torr7s <torresmiranda.rj@gmail.com>
2022-03-04 22:26:37 -0300

    Singleton to user's repository created

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from 'tsyringe';
 
 import { ICarCategoriesRepository } from '../../modules/cars/repositories/CarCategoriesInterface'
 import { ICarSpecificationsRepository } from '../../modules/cars/repositories/CarSpecificationsInterface';
+import { IUsersRepository } from '../../modules/users/repositories/UsersInterface';
 
 import { CarCategoriesRepository } from '../../modules/cars/repositories/implementations/CarCategoriesRepository';
 import { CarSpecificationsRepository } from '../../modules/cars/repositories/implementations/CarSpecificationsRepository';
+import { UsersRepository } from '../../modules/users/repositories/implementations/UsersRepository';
 
 container.registerSingleton<ICarCategoriesRepository>(
   'CarCategoriesRepository',
@@ -14,4 +16,9 @@ container.registerSingleton<ICarCategoriesRepository>(
 container.registerSingleton<ICarSpecificationsRepository>(
   'CarSpecificationsRepository',
   CarSpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 )
