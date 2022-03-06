e7ada4dec33623c43ddf54cc202ef04173d63afb
Torr7s <torresmiranda.rj@gmail.com>
2022-03-06 20:56:52 -0300

    Update users.routes.ts

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,26 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import { AuthUserMiddleware } from '../middlewares/auth-user.middleware';
+
 const userRouter = Router()
 
-import { CreateUserController } from '../modules/users/useCases/createUser/CreateUserController';
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'))
 
-const CreateUserHandler = new CreateUserController().handle
+import { CreateUserController } from '../modules/users/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/users/useCases/updateUserAvatar/UpdateUserAvatarController';
+
+const CreateUserHandler       = new CreateUserController().handle
+const UpdateUserAvatarHandler = new UpdateUserAvatarController().handler
 
 userRouter.post('/', CreateUserHandler)
+
+userRouter.patch(
+  '/avatar', 
+  AuthUserMiddleware, 
+  uploadAvatar.single('avatar'), 
+  UpdateUserAvatarHandler
+)
 
 export { userRouter }
