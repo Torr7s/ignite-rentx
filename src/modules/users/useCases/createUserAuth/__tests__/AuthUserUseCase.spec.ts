3811b5227291197077944edd1fe246de0a5257e8
Torr7s <torresmiranda.rj@gmail.com>
2022-03-14 12:53:21 -0300

    Create AuthUserUseCase.spec.ts
    User authentication test

---
+++ src/modules/users/useCases/createUserAuth/__tests__/AuthUserUseCase.spec.ts
@@ -0,0 +1,71 @@
+import { AppError } from '../../../../../errors/app.error'
+
+import { AuthUserUseCase, IAuthUserResponse } from '../AuthUserUseCase'
+import { CreateUserUseCase } from '../../createUser/CreateUserUseCase'
+
+import { UsersRepositoryInMemory } from '../../../repositories/in-memory/UsersRepositoryInMemory'
+
+import { ICreateUserDto } from '../../../dtos/CreateUserDto'
+
+let usersRepositoryInMemory: UsersRepositoryInMemory
+let authUserUseCase: AuthUserUseCase
+let createUserUseCase: CreateUserUseCase
+
+describe('Authenticate user', () => {
+  beforeAll(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory()
+    authUserUseCase = new AuthUserUseCase(
+      usersRepositoryInMemory
+    )
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
+  })
+
+  it('should be able to authenticate an user', async () => {
+    const userData: ICreateUserDto = {
+      name: 'User 1',
+      email: 'usertest@gmail.com',
+      password: 'test123',
+      driver_license: '000123',
+    }
+
+    await createUserUseCase.perform(userData)
+
+    const authResult: IAuthUserResponse = await authUserUseCase.perform({
+      email: userData.email,
+      password: userData.password
+    })
+
+    expect(authResult).toHaveProperty('token')
+  })
+
+  it('should not be able to authenticate an non-existent user', async () => {
+    expect(async () => {
+      await authUserUseCase.perform({
+        email: 'fake@gmail.com',
+        password: '1234'
+      })
+    })
+      .rejects
+      .toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to authenticate an user with incorret password', async () => {
+    expect(async () => {
+      const userData: ICreateUserDto = {
+        name: 'User 2',
+        email: 'usertest2@gmail.com',
+        password: 'test1234',
+        driver_license: '09999',
+      }
+
+      await createUserUseCase.perform(userData)
+
+      await authUserUseCase.perform({
+        email: userData.email,
+        password: 'incorrect_password'
+      })
+    })
+      .rejects
+      .toBeInstanceOf(AppError)
+  })
+})
