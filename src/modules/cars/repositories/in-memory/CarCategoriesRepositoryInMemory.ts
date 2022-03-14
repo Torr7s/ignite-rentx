b0160c901473f94ada62e8e79fb6bd0f59eb6112
Torr7s <torresmiranda.rj@gmail.com>
2022-03-14 12:52:48 -0300

    In memory repositories added

---
+++ src/modules/cars/repositories/in-memory/CarCategoriesRepositoryInMemory.ts
@@ -0,0 +1,32 @@
+import { CarCategoryEntity } from '../../entities/CarCategoryEntity';
+
+import { ICarCategoriesRepository, ICreateCarCategoryDto } from '../CarCategoriesInterface';
+
+class CarCategoriesRepositoryInMemory implements ICarCategoriesRepository {
+  private categories: CarCategoryEntity[] = []
+  
+  async create({ name, description }: ICreateCarCategoryDto): Promise<void> {
+    const newCategoryData: CarCategoryEntity = new CarCategoryEntity()
+    
+    Object.assign(newCategoryData, {
+      name,
+      description
+    })
+
+    this.categories.push(newCategoryData)
+  }
+
+  async list(): Promise<CarCategoryEntity[]> {
+    const categoriesData: CarCategoryEntity[] = this.categories
+
+    return categoriesData
+  }
+
+  async findByName(name: string): Promise<CarCategoryEntity> {
+    const categoryData: CarCategoryEntity = this.categories.find((category) => category.name === name)
+
+    return categoryData
+  }
+}
+
+export { CarCategoriesRepositoryInMemory }
---
+++ src/modules/users/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,43 @@
+import { UserEntity } from '../../entities/UserEntity';
+
+import { ICreateUserDto } from '../../dtos/CreateUserDto';
+import { IUsersRepository } from '../UsersInterface';
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  private users: UserEntity[] = []
+
+  async create({ name, email, password, driver_license }: ICreateUserDto): Promise<void> {
+    const newUserData: UserEntity = new UserEntity()
+
+    Object.assign(newUserData, {
+      name,
+      email,
+      password,
+      driver_license
+    })
+
+    this.users.push(newUserData)
+  }
+
+  async findByEmail(email: string): Promise<UserEntity> {
+    const userData: UserEntity = this.users.find((user) => user.email === email)
+
+    return userData
+  }
+
+  async findById(id: string): Promise<UserEntity> {
+    const userData: UserEntity = this.users.find((user) => user.id === id)
+
+    return userData
+  }
+
+  async updateAvatar(id: string, avatar_file: string): Promise<UserEntity> {
+    const userData = this.findById(id)
+
+    userData['avatar'] = avatar_file
+
+    return userData
+  }
+}
+
+export { UsersRepositoryInMemory }
