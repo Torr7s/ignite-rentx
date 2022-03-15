78d091b474c866091cfdf099e50a3ac61e0987c3
Torr7s <torresmiranda.rj@gmail.com>
2022-03-15 20:59:34 -0300

    Update index.ts
    Path and car route refactoring added

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
-import { carCategoryRouter } from './car-categories.routes';
-import { carSpecificationRouter } from './car-specifications.routes';
 import { userRouter } from './users.routes';
 import { authUserRouter } from './auth-user.routes';
 
+import { carRouter } from './cars.routes';
+import { carCategoryRouter } from './car-categories.routes';
+import { carSpecificationRouter } from './car-specifications.routes';
+
 const router = Router()
 
-router.use('/car/categories', carCategoryRouter)
-router.use('/car/specifications', carSpecificationRouter)
-router.use('/users', userRouter)
+router.use('/api/users', userRouter)
 router.use(authUserRouter)
+
+router.use('/api/cars', carRouter)
+router.use('/api/cars/categories', carCategoryRouter)
+router.use('/api/cars/specifications', carSpecificationRouter)
 
 export { router }
