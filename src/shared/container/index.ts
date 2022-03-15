5ef58e65ed1eecfeed19308091758d405f54f4e0
Torr7s <torresmiranda.rj@gmail.com>
2022-03-15 20:56:06 -0300

    Update index.ts
    Cars repository singleton created

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,12 +1,19 @@
 import { container } from 'tsyringe';
 
+import { ICarsRepository } from '@modules/cars/repositories/CarsInterface';
 import { ICarCategoriesRepository } from '@modules/cars/repositories/CarCategoriesInterface';
 import { ICarSpecificationsRepository } from '@modules/cars/repositories/CarSpecificationsInterface';
 import { IUsersRepository } from '@modules/users/repositories/UsersInterface';
 
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CarCategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CarCategoriesRepository';
 import { CarSpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/CarSpecificationsRepository';
 import { UsersRepository } from '@modules/users/infra/repositories/UsersRepository';
+
+container.registerSingleton<ICarsRepository>(
+  'CarsRepository',
+  CarsRepository
+)
 
 container.registerSingleton<ICarCategoriesRepository>(
   'CarCategoriesRepository',
