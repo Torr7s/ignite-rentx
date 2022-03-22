a09c3d7b57a11fa09ef3f4949227fb338ea325fa
Torr7s <torresmiranda.rj@gmail.com>
2022-03-22 17:18:56 -0300

    Update CarsInterface.ts
    Added new findById method

--- src/modules/cars/repositories/CarsInterface.ts
+++ src/modules/cars/repositories/CarsInterface.ts
@@ -3,12 +3,13 @@ import { CarEntity } from '@modules/cars/infra/typeorm/entities/CarEntity';
 
 interface ICarsRepository {
   create(data: ICreateCarDto): Promise<CarEntity>;
-  findByLicensePlate(license_plate: string): Promise<CarEntity>;
   findAvailableCars(
     name?: string,
     brand?: string,
     category_id?: string
-  ): Promise<CarEntity[]>;
+    ): Promise<CarEntity[]>;
+    findById(car_id: string): Promise<CarEntity>;
+    findByLicensePlate(license_plate: string): Promise<CarEntity>;
 }
 
 export { ICarsRepository }
