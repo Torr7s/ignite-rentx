948f8c2f002d94f6b67f5c06866f7b178f999d31
Torr7s <torresmiranda.rj@gmail.com>
2022-03-22 17:08:24 -0300

    Added route to specification creation

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -4,18 +4,27 @@ import { AuthUserMiddleware } from '@shared/infra/http/middlewares/auth-user.mid
 import { AdminMiddleware } from '@shared/infra/http/middlewares/admin.middleware';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
-import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/listAvailableCarsController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 
 const carRouter = Router()
 
-const CreateCarHandler         = new CreateCarController().handle
-const ListAvailableCarsHandler = new ListAvailableCarsController().handle
+const CreateCarHandler              = new CreateCarController().handle
+const CreateCarSpecificationHandler = new CreateCarSpecificationController().handle
+const ListAvailableCarsHandler      = new ListAvailableCarsController().handle
 
 carRouter.post(
   '/', 
   AuthUserMiddleware, 
   AdminMiddleware, 
   CreateCarHandler
+)
+
+carRouter.post(
+  '/specifications/:id',
+  AuthUserMiddleware,
+  AdminMiddleware,
+  CreateCarSpecificationHandler
 )
 
 carRouter.get('/available', ListAvailableCarsHandler)
