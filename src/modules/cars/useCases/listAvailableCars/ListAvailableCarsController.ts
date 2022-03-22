03902be8d1080e31dc085a75b7e6a789af53872d
Torr7s <torresmiranda.rj@gmail.com>
2022-03-22 17:11:37 -0300

    Update listAvailableCars use case

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsController.ts
@@ -0,0 +1,23 @@
+import { container } from 'tsyringe';
+import { Request, Response } from 'express';
+
+import { IListAvailableCarsRequest, ListAvailableCarsUseCase } from './ListAvailableCarsUseCase';
+import { CarEntity } from '@modules/cars/infra/typeorm/entities/CarEntity';
+
+class ListAvailableCarsController {
+  async handle(request: Request, response: Response): Promise<Response> { 
+    const listAvailableCarsUseCase = container.resolve(ListAvailableCarsUseCase)
+
+    const { name, brand, category_id }: IListAvailableCarsRequest = request.query
+
+    const availableCars: CarEntity[] = await listAvailableCarsUseCase.perform({
+      name,
+      brand,
+      category_id
+    })
+
+    return response.json(availableCars)
+  }
+}
+
+export { ListAvailableCarsController }
---
+++ src/modules/cars/useCases/listAvailableCars/__tests__/ListAvailableCarsUseCase.spec.ts
@@ -0,0 +1,84 @@
+import { CarEntity } from '@modules/cars/infra/typeorm/entities/CarEntity';
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { ListAvailableCarsUseCase } from '../ListAvailableCarsUseCase';
+
+let listAvailableCarsUseCase: ListAvailableCarsUseCase
+let carsRepositoryInMemory: CarsRepositoryInMemory
+
+describe('List cars', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory()
+    listAvailableCarsUseCase = new ListAvailableCarsUseCase(carsRepositoryInMemory)
+  })
+
+  it('should be able to list all available cars', async () => {
+    const newCarData: CarEntity = await carsRepositoryInMemory.create({
+      'name': 'test_car',
+      'description': 'test_car_desc',
+      'daily_rate': 111,
+      'license_plate': 'DEF-1234',
+      'fine_amount': 111,
+      'brand': 'test_brand',
+      'category_id': 'test_category'
+    })
+
+    const cars: CarEntity[] = await listAvailableCarsUseCase.perform({})
+
+    expect(cars).toEqual([newCarData])
+  })
+
+  it('should be able to list all available cars by name', async () => {
+    const newCarData: CarEntity = await carsRepositoryInMemory.create({
+      'name': 'test_car_2',
+      'description': 'test_car_desc_2',
+      'daily_rate': 222,
+      'license_plate': 'DEF-1234',
+      'fine_amount': 222,
+      'brand': 'test_brand_2',
+      'category_id': 'test_category_2'
+    })
+
+    const cars: CarEntity[] = await listAvailableCarsUseCase.perform({
+      name: 'test_car_2'
+    })
+
+    expect(cars).toEqual([newCarData])
+  })
+
+  it('should be able to list all available cars by brand', async () => {
+    const newCarData: CarEntity = await carsRepositoryInMemory.create({
+      'name': 'test_car_3',
+      'description': 'test_car_desc_3',
+      'daily_rate': 333,
+      'license_plate': 'DEF-1234',
+      'fine_amount': 333,
+      'brand': 'test_brand_3',
+      'category_id': 'test_category_3'
+    })
+
+    const cars: CarEntity[] = await listAvailableCarsUseCase.perform({
+      brand: 'test_brand_3'
+    })
+
+    expect(cars).toEqual([newCarData])
+  })
+
+  it('should be able to list all available cars by category', async () => {
+    const newCarData: CarEntity = await carsRepositoryInMemory.create({
+      'name': 'test_car_4',
+      'description': 'test_car_desc_4',
+      'daily_rate': 444,
+      'license_plate': 'DEF-1234',
+      'fine_amount': 444,
+      'brand': 'test_brand_4',
+      'category_id': 'test_category_4'
+    })
+
+    const cars: CarEntity[] = await listAvailableCarsUseCase.perform({
+      category_id: 'test_category_4'
+    })
+
+    expect(cars).toEqual([newCarData])
+  })
+})
