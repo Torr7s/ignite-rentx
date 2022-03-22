5ce29e5f8ba7f3ed948dedb79e99304c286e7e53
Torr7s <torresmiranda.rj@gmail.com>
2022-03-22 17:16:46 -0300

    Added some properties for creating a car

--- src/modules/cars/dtos/CreateCarDto.ts
+++ src/modules/cars/dtos/CreateCarDto.ts
@@ -1,11 +1,15 @@
+import { SpecificationEntity } from '@modules/cars/infra/typeorm/entities/SpecificationEntity';
+
 interface ICreateCarDto {
-  name,
-  description,
-  daily_rate,
-  license_plate,
-  fine_amount,
-  brand,
-  category_id
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+  specifications?: SpecificationEntity[];
+  id?: string;
 }
 
 export { ICreateCarDto }
