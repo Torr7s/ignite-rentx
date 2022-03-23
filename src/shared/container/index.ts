8d078d3e5c2d355a07210a0c64bd71e720c72cf7
Torr7s <torresmiranda.rj@gmail.com>
2022-03-23 14:01:49 -0300

    Update index.ts
    Added CarImagesRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,13 @@
 import { container } from 'tsyringe';
 
 import { ICarsRepository } from '@modules/cars/repositories/CarsInterface';
+import { ICarImagesRepository } from '@modules/cars/repositories/CarImagesInterface';
 import { ICategoriesRepository } from '@modules/cars/repositories/CategoriesInterface';
 import { ISpecificationsRepository } from '@modules/cars/repositories/SpecificationsInterface';
 import { IUsersRepository } from '@modules/users/repositories/UsersInterface';
 
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
+import { CarImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarImagesRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { UsersRepository } from '@modules/users/infra/repositories/UsersRepository';
@@ -13,6 +15,11 @@ import { UsersRepository } from '@modules/users/infra/repositories/UsersReposito
 container.registerSingleton<ICarsRepository>(
   'CarsRepository',
   CarsRepository
+)
+
+container.registerSingleton<ICarImagesRepository>(
+  'CarImagesRepository',
+  CarImagesRepository
 )
 
 container.registerSingleton<ICategoriesRepository>(
