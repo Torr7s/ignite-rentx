aa989743f3d985ec1811b050a0148647a37d947c
Torr7s <torresmiranda.rj@gmail.com>
2022-03-23 13:59:22 -0300

    Create CarImagesRepository.ts

---
+++ src/modules/cars/infra/typeorm/repositories/CarImagesRepository.ts
@@ -0,0 +1,25 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { CarImagesEntity } from '../entities/CarImagesEntity';
+import { ICarImagesRepository } from '@modules/cars/repositories/CarImagesInterface';
+
+class CarImagesRepository implements ICarImagesRepository {
+  private _carImagesRepository: Repository<CarImagesEntity>
+
+  constructor() {
+    this._carImagesRepository = getRepository(CarImagesEntity)
+  }
+  
+  async create(car_id: string, image_name: string): Promise<CarImagesEntity> {
+    const carImageData: CarImagesEntity = this._carImagesRepository.create({
+      car_id,
+      image_name
+    })
+
+    await this._carImagesRepository.save(carImageData)
+
+    return carImageData
+  }
+}
+
+export { CarImagesRepository }
