2772b3b835c07789aaea0eba2a34ee0093d5adbe
Torr7s <torresmiranda.rj@gmail.com>
2022-03-24 12:11:59 -0300

    Update index.ts
    Added route to create a rental

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import { userRouter } from './users.routes';
 import { authRouter } from './auth.routes';
 
 import { carRouter } from './cars.routes';
+import { rentalRouter } from './rental.routes';
 import { categoryRouter } from './categories.routes';
 import { specificationRouter } from './specifications.routes';
 
@@ -13,6 +14,7 @@ router.use('/api/users', userRouter)
 router.use(authRouter)
 
 router.use('/api/cars', carRouter)
+router.use('/api/rentals', rentalRouter)
 router.use('/api/categories', categoryRouter)
 router.use('/api/specifications', specificationRouter)
 
