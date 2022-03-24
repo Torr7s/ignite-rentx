60b1a6d2207b4569938bb99460f981708a7e83dd
Torr7s <torresmiranda.rj@gmail.com>
2022-03-24 12:09:37 -0300

    Update index.ts
    Registered Rentals Repository as singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,15 +1,19 @@
 import { container } from 'tsyringe';
 
+import '@shared/container/providers';
+
 import { ICarsRepository } from '@modules/cars/repositories/CarsInterface';
 import { ICarImagesRepository } from '@modules/cars/repositories/CarImagesInterface';
 import { ICategoriesRepository } from '@modules/cars/repositories/CategoriesInterface';
 import { ISpecificationsRepository } from '@modules/cars/repositories/SpecificationsInterface';
+import { IRentalsRepository } from '@modules/rentals/repositories/RentalsInterface';
 import { IUsersRepository } from '@modules/users/repositories/UsersInterface';
 
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CarImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarImagesRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { RentalsRepository } from '@modules/rentals/infra/typeorm/repositories/RentalsRepository';
 import { UsersRepository } from '@modules/users/infra/repositories/UsersRepository';
 
 container.registerSingleton<ICarsRepository>(
@@ -30,6 +34,11 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   'SpecificationsRepository',
   SpecificationsRepository
+)
+
+container.registerSingleton<IRentalsRepository>(
+  'RentalsRepository',
+  RentalsRepository
 )
 
 container.registerSingleton<IUsersRepository>(
