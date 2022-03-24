e73666a66aaeab098e8c92190534d59c941a3e4f
Torr7s <torresmiranda.rj@gmail.com>
2022-03-24 12:07:15 -0300

    File Restructuring

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,33 @@
+import swaggerUI from 'swagger-ui-express';
+import swaggerFile from '../../../swagger.json';
+
+import { router } from './routes';
+
+import express, { NextFunction, Request, Response } from 'express';
+
+import { AppError } from '@shared/errors';
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
+
+app.use(router)
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        error: err.message
+      })
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      error: 'Internal server error'
+    })
+  }
+)
+
+export { app }
