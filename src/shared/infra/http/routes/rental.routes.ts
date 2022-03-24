28da6afa6f7f991abb71e6b6dba20f6beaf7bc9a
Torr7s <torresmiranda.rj@gmail.com>
2022-03-24 12:12:02 -0300

    Create rental.routes.ts

---
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import { AuthUserMiddleware } from '../middlewares/auth-user.middleware';
+
+import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
+
+const rentalRouter = Router()
+
+const CreateRentalHandler = new CreateRentalController().handle
+
+rentalRouter.post('/', AuthUserMiddleware, CreateRentalHandler)
+
+export { rentalRouter }
