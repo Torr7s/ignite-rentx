5e29750d5c76a639f6b11b52c5fb0df6e5817657
Torr7s <torresmiranda.rj@gmail.com>
2022-03-24 23:16:00 -0300

    Update server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import '@shared/container';
 
-import { app } from 'app';
+import { app } from './app';
 
 import createConnection from '@shared/infra/typeorm';
 
