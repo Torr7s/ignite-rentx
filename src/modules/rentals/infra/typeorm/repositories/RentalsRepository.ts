e39421fc85cdb057e1ff3e1348ec7b059d4c242f
Torr7s <torresmiranda.rj@gmail.com>
2022-04-27 16:21:25 -0300

    [refactor]: Updated methods for greater accuracy of results

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -6,16 +6,19 @@ import { ICreateRentalDto } from '@modules/rentals/domain/dtos/CreateRentalDto';
 
 class RentalsRepository implements IRentalsRepository {
   private _rentalsRepository: Repository<RentalEntity>
-  
+
   constructor() {
     this._rentalsRepository = getRepository(RentalEntity)
   }
 
-  async create({ user_id, car_id, expected_return_date }: ICreateRentalDto): Promise<RentalEntity> {
+  async create({ id, car_id, user_id, end_date, expected_return_date, total }: ICreateRentalDto): Promise<RentalEntity> {
     const rentalData: RentalEntity = this._rentalsRepository.create({
-      user_id,
+      id,
       car_id,
-      expected_return_date
+      user_id,
+      end_date,
+      expected_return_date,
+      total
     })
 
     await this._rentalsRepository.save(rentalData)
@@ -23,14 +26,30 @@ class RentalsRepository implements IRentalsRepository {
     return rentalData
   }
 
+  async findById(id: string): Promise<RentalEntity> {
+    const rentalData: RentalEntity = await this._rentalsRepository.findOne(id)
+
+    return rentalData
+  }
+
   async findRentalByCarId(car_id: string): Promise<RentalEntity> {
-    const openRentalByCar: RentalEntity = await this._rentalsRepository.findOne({ car_id })
+    const openRentalByCar: RentalEntity = await this._rentalsRepository.findOne({
+      where: {
+        car_id,
+        end_date: null
+      }
+    })
 
     return openRentalByCar
   }
 
   async findRentalByUserId(user_id: string): Promise<RentalEntity> {
-    const openRentalByUser: RentalEntity = await this._rentalsRepository.findOne({ user_id })
+    const openRentalByUser: RentalEntity = await this._rentalsRepository.findOne({
+      where: {
+        user_id,
+        end_date: null
+      }
+    })
 
     return openRentalByUser
   }
