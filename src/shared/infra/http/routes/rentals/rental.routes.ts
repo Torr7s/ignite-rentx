2670388245a36dd8f6c10dceb10350bbb691cd39
Torr7s <torresmiranda.rj@gmail.com>
2022-04-28 09:38:18 -0300

    ListUserRentals route implementation

--- src/shared/infra/http/routes/rentals/rental.routes.ts
+++ src/shared/infra/http/routes/rentals/rental.routes.ts
@@ -4,13 +4,16 @@ import { AuthUserMiddleware } from '../../middlewares/auth-user.middleware';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListUserRentalsController } from '@modules/rentals/useCases/listUserRentals/ListUserRentalsController';
 
 const rentalRouter = Router()
 
 const CreateRentalHandler     = new CreateRentalController().handle
 const DevolutionRentalHandler = new DevolutionRentalController().handle
+const ListUserRentalsHandler  = new ListUserRentalsController().handle
 
 rentalRouter.post('/', AuthUserMiddleware, CreateRentalHandler)
 rentalRouter.post('/devolution/:id', AuthUserMiddleware, DevolutionRentalHandler)
+rentalRouter.get('/', AuthUserMiddleware, ListUserRentalsHandler)
 
 export { rentalRouter }
