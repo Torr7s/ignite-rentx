043a39bc92bf9ef84f48664dab30662b22d42607
Torr7s <torresmiranda.rj@gmail.com>
2022-04-29 14:13:59 -0300

    Applying methods implementation in the repository

--- src/modules/users/domain/repositories/UsersTokensInterface.ts
+++ src/modules/users/domain/repositories/UsersTokensInterface.ts
@@ -2,7 +2,9 @@ import { UserTokensEntity } from '@modules/users/infra/typeorm/entities/UserToke
 import { ICreateUserTokenDto } from '../dtos/CreateUserTokenDto';
 
 interface IUsersTokensRepository {
-  create(data: ICreateUserTokenDto): Promise<UserTokensEntity>
+  create(data: ICreateUserTokenDto): Promise<UserTokensEntity>;
+  deleteById(id: string): Promise<void>;
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokensEntity>;
 }
 
 export { IUsersTokensRepository }
--- src/modules/users/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -22,6 +22,19 @@ class UsersTokensRepository implements IUsersTokensRepository {
 
     return newUserTokenData
   }
+
+  async deleteById(id: string): Promise<void> {
+    await this._usersTokensRepository.delete({ id })
+  }
+
+  async findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokensEntity> {
+    const usersTokenData: UserTokensEntity = await this._usersTokensRepository.findOne({
+      user_id,
+      refresh_token
+    })
+
+    return usersTokenData
+  }
 }
 
 export { UsersTokensRepository }
