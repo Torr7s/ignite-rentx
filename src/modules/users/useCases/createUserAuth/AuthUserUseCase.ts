2c99023b2b7891da02ce9d51f6003e257d8bb024
Torr7s <torresmiranda.rj@gmail.com>
2022-04-29 14:15:03 -0300

    Refactored code to use refresh token

--- src/modules/users/useCases/createUserAuth/AuthUserUseCase.ts
+++ src/modules/users/useCases/createUserAuth/AuthUserUseCase.ts
@@ -6,8 +6,11 @@ import { inject, injectable } from 'tsyringe';
 import { UserEntity } from '@modules/users/infra/typeorm/entities/UserEntity';
 
 import { IUsersRepository } from '@modules/users/domain/repositories/UsersInterface';
+import { IUsersTokensRepository } from '@modules/users/domain/repositories/UsersTokensInterface';
 
 import { AppError } from '@shared/errors';
+
+import { IDateProvider } from '@shared/container/providers/dateProvider/DateProviderInterface';
 
 interface IAuthUserRequest {
   email: string;
@@ -20,17 +23,22 @@ interface IAuthUserResponse {
     email: string
   },
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
 class AuthUserUseCase {
   constructor(
     @inject('UsersRepository')
-    private _repository: IUsersRepository
+    private _usersRepository: IUsersRepository,
+    @inject('UsersTokensRepository')
+    private _usersTokensRepository: IUsersTokensRepository,
+    @inject('DayjsDateProvider')
+    private _dateProvider: IDateProvider
   ) { }
 
   async perform({ email, password }: IAuthUserRequest): Promise<IAuthUserResponse> {
-    const userData: UserEntity = await this._repository.findByEmail(email)
+    const userData: UserEntity = await this._usersRepository.findByEmail(email)
 
     if (!userData) throw new AppError('Invalid credentials!', 401)
 
@@ -38,9 +46,24 @@ class AuthUserUseCase {
 
     if (!validPassword) throw new AppError('Invalid credentials!', 401)
 
-    const token = sign({}, `${process.env.MD5_HASH}`, {
+    const { MD5_HASH, REFRESH_TOKEN_HASH } = process.env
+
+    const token = sign({}, MD5_HASH, {
       subject: userData.id,
       expiresIn: '1d'
+    })
+
+    const refresh_token = sign({ email }, REFRESH_TOKEN_HASH, {
+      subject: userData.id,
+      expiresIn: '30d'
+    })
+
+    const expiration_date = this._dateProvider.addDays(30)
+
+    await this._usersTokensRepository.create({
+      user_id: userData.id,
+      refresh_token,
+      expiration_date
     })
 
     const tokenData: IAuthUserResponse = {
@@ -48,7 +71,8 @@ class AuthUserUseCase {
       userData: {
         name: userData.name,
         email: userData.email
-      }
+      },
+      refresh_token
     }
 
     return tokenData
