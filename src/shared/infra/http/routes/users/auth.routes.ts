57bc39b94ecc0e961b7818a052df5f56d65e48a0
Torr7s <torresmiranda.rj@gmail.com>
2022-04-29 14:19:24 -0300

    Create route to refresh tokens

--- src/shared/infra/http/routes/users/auth.routes.ts
+++ src/shared/infra/http/routes/users/auth.routes.ts
@@ -3,9 +3,12 @@ import { Router } from 'express';
 const authRouter = Router()
 
 import { AuthUserController } from '@modules/users/useCases/createUserAuth/AuthUserController';
+import { RefreshTokenController } from '@modules/users/useCases/refreshToken/RefreshTokenController';
 
-const AuthUserHandler = new AuthUserController().handle
+const AuthUserHandler     = new AuthUserController().handle
+const RefreshTokenHandler = new RefreshTokenController().handle
 
 authRouter.post('/login', AuthUserHandler)
+authRouter.post('/refresh-token', RefreshTokenHandler)
 
 export { authRouter }
